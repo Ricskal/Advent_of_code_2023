868ebf9eacc99efaee54b272f463f5bfb3168872
ricskal <rickensinkrullen@gmail.com>
2023-12-04 10:46:51 +0100

    dag 1

--- 1.2 Day_ett_ett.py
+++ 1.2 Day_ett_ett.py
@@ -1,4 +1,3 @@
-
 # The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 #
 # For example:
@@ -27,23 +26,43 @@
 # In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
 answer = 0
 
-
 file = open('Inputs/Day_1_input', 'r')
 line_list = file.readlines()
 fline_list = []
 fline_list_list = []
+number_index = {}
+
+number_dict = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+number_list = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+number_list1 = ['one', '1', 'two', '2', 'three', '3', 'four', '4', 'five', '5', 'six', '6', 'seven', '7', 'eight', '8', 'nine', '9']
+
 
 for line in line_list:
-    fline_list = []
-    for char in line:
-        if char.isdigit():
-            fline_list.append(char)
-    fline_list_list.append(fline_list)
+    for number in number_list1:
+        length = len(line)
+        index_start = 0
+        i = 0
+        while i < length:
+            index = line.find(number, index_start)
+            if index != -1:
+                if not number.isdigit():
+                    number1 = number_dict[number]
+                    number_index[index] = number1
+                else:
+                    number_index[index] = number
+            index_start += 1
+            i += 1
 
-for flist in fline_list_list:
-    first_digit = flist[0]
-    last_digit = flist[-1]
+    number_index_sorted = dict(sorted(number_index.items()))
+
+    i = list(number_index_sorted.values())
+    first_digit = i[0]
+    last_digit = i[-1]
     total_string = first_digit + last_digit
+    print(f'Het antwoord was {answer}, het is nu: first = {first_digit} + last = {last_digit}')
     answer += int(total_string)
+
+    number_index.clear()
+    number_index_sorted.clear()
 
 print(answer)
