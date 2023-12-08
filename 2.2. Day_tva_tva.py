f3366070a2a60c0f22355662b181cce9290bcfa2
ricskal <rickensinkrullen@gmail.com>
2023-12-08 18:30:57 +0100

    dag 5

--- 2.2. Day_tva_tva.py
+++ 2.2. Day_tva_tva.py
@@ -1,3 +1,4 @@
+# --- Day 2: Cube Conundrum ---
 # As you walk, the Elf shows you a small bag and some cubes which are either red, green, or blue. Each time you play this game, he will hide a secret number of cubes of each color in the bag, and your goal is to figure out information about the number of cubes.
 #
 # To get information, once a bag has been loaded with cubes, the Elf will reach into the bag, grab a handful of random cubes, show them to you, and then put them back in the bag. He'll do this a few times per game.
--- 5. Day_fem.py
+++ 5. Day_fem.py
@@ -1,45 +1,182 @@
-# The Elf leads you over to the pile of colorful cards. There, you discover dozens of scratchcards, all with their opaque covering already scratched off. Picking one up, it looks like each card has two lists of numbers separated by a vertical bar (|): a list of winning numbers and then a list of numbers you have. You organize the information into a table (your puzzle input).
+# --- Day 5: If You Give A Seed A Fertilizer ---
 #
-# As far as the Elf has been able to figure out, you have to figure out which of the numbers you have appear in the list of winning numbers. The first match makes the card worth one point and each match after the first doubles the point value of that card.
+# The almanac (your puzzle input) lists all of the seeds that need to be planted. It also lists what type of soil to use with each kind of seed, what type of fertilizer to use with each kind of soil, what type of water to use with each kind of fertilizer, and so on. Every type of seed, soil, fertilizer and so on is identified with a number, but numbers are reused by each category - that is, soil 123 and fertilizer 123 aren't necessarily related to each other.
 #
 # For example:
 #
-# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+# seeds: 79 14 55 13
 #
-# In the above example, card 1 has five winning numbers (41, 48, 83, 86, and 17) and eight numbers you have (83, 86, 6, 31, 17, 9, 48, and 53). Of the numbers you have, four of them (48, 83, 17, and 86) are winning numbers! That means card 1 is worth 8 points (1 for the first match, then doubled three times for each of the three matches after the first).
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
 #
-#     Card 2 has two winning numbers (32 and 61), so it is worth 2 points.
-#     Card 3 has two winning numbers (1 and 21), so it is worth 2 points.
-#     Card 4 has one winning number (84), so it is worth 1 point.
-#     Card 5 has no winning numbers, so it is worth no points.
-#     Card 6 has no winning numbers, so it is worth no points.
+# soil-to-fertilizer map:
+# 0 15 37
+# 37 52 2
+# 39 0 15
 #
-# So, in this example, the Elf's pile of scratchcards is worth 13 points.
+# fertilizer-to-water map:
+# 49 53 8
+# 0 11 42
+# 42 0 7
+# 57 7 4
 #
-# Take a seat in the large pile of colorful cards. How many points are they worth in total?
+# water-to-light map:
+# 88 18 7
+# 18 25 70
+#
+# light-to-temperature map:
+# 45 77 23
+# 81 45 19
+# 68 64 13
+#
+# temperature-to-humidity map:
+# 0 69 1
+# 1 0 69
+#
+# humidity-to-location map:
+# 60 56 37
+# 56 93 4
+#
+# The almanac starts by listing which seeds need to be planted: seeds 79, 14, 55, and 13.
+#
+# The rest of the almanac contains a list of maps which describe how to convert numbers from a source category into numbers in a destination category. That is, the section that starts with seed-to-soil map: describes how to convert a seed number (the source) to a soil number (the destination). This lets the gardener and his team know which soil to use with which seeds, which water to use with which fertilizer, and so on.
+#
+# Rather than list every source number and its corresponding destination number one by one, the maps describe entire ranges of numbers that can be converted. Each line within a map contains three numbers: the destination range start, the source range start, and the range length.
+#
+# Consider again the example seed-to-soil map:
+#
+# 50 98 2
+# 52 50 48
+#
+# The first line has a destination range start of 50, a source range start of 98, and a range length of 2. This line means that the source range starts at 98 and contains two values: 98 and 99. The destination range is the same length, but it starts at 50, so its two values are 50 and 51. With this information, you know that seed number 98 corresponds to soil number 50 and that seed number 99 corresponds to soil number 51.
+#
+# The second line means that the source range starts at 50 and contains 48 values: 50, 51, ..., 96, 97. This corresponds to a destination range starting at 52 and also containing 48 values: 52, 53, ..., 98, 99. So, seed number 53 corresponds to soil number 55.
+#
+# Any source numbers that aren't mapped correspond to the same destination number. So, seed number 10 corresponds to soil number 10.
+#
+# So, the entire list of seed numbers and their corresponding soil numbers looks like this:
+#
+# seed  soil
+# 0     0
+# 1     1
+# ...   ...
+# 48    48
+# 49    49
+# 50    52
+# 51    53
+# ...   ...
+# 96    98
+# 97    99
+# 98    50
+# 99    51
+#
+# With this map, you can look up the soil number required for each initial seed number:
+#
+#     Seed number 79 corresponds to soil number 81.
+#     Seed number 14 corresponds to soil number 14.
+#     Seed number 55 corresponds to soil number 57.
+#     Seed number 13 corresponds to soil number 13.
+#
+# The gardener and his team want to get started as soon as possible, so they'd like to know the closest location that needs a seed. Using these maps, find the lowest location number that corresponds to any of the initial seeds. To do this, you'll need to convert each seed number through other categories until you can find its corresponding location number. In this example, the corresponding types are:
+#
+#     Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+#     Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
+#     Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
+#     Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
+#
+# So, the lowest location number in this example is 35.
+#
+# What is the lowest location number that corresponds to any of the initial seed numbers?
 
 answer = 0
 score = 0
-winning_nr_list = ()
-scratched_nr_list = ()
-file = open('Inputs/Day_4_input', 'r')
+file = open('Inputs/Day_5_test_input', 'r')
 line_list = file.readlines()
 
+seed_list = list()
+seed_to_soil_started = False
+seed_to_soil_map = list()
+soil_to_fertilizer_started = False
+soil_to_fertilizer_map = list()
+fertilizer_to_water_started = False
+fertilizer_to_water_map = list()
+water_to_light_started = False
+water_to_light_map = list()
+light_to_temperature_started = False
+light_to_temperature_map = list()
+temperature_to_humidity_started = False
+temperature_to_humidity_map = list()
+humidity_to_location_map = list()
+
+while '\n' in line_list:
+    line_list.remove('\n')
+print(line_list)
+
 for line in line_list:
-    line_split = line.replace(':', '|').strip().split('|')
-    card_nr = line_split[0].split(' ')[1]
-    winning_nr_list = line_split[1].strip().replace('  ',' ').split(' ')
-    scratched_nr_list = line_split[2].strip().replace('  ',' ').split(' ')
-    print(f'Card number = {card_nr}, it has the winning numbers = {winning_nr_list} and the following scratched numbers = {scratched_nr_list}')
-    x = [value for value in winning_nr_list if value in scratched_nr_list]  # uitzoeken
-    y = len(x)-1
-    if y >= 0:
-        score = 2 ** y
-        answer += score
-print(answer)
+    x = line.strip().split(' ')
+
+    if x[0] == 'seeds:':
+        seed_list = x[1::]
+
+    elif x[0] == 'seed-to-soil':
+        seed_to_soil_started = True
+        continue
+    elif seed_to_soil_started and x[0] != 'soil-to-fertilizer':
+        seed_to_soil_map.append(x)
+
+    elif x[0] == 'soil-to-fertilizer':
+        seed_to_soil_started = False
+        soil_to_fertilizer_started = True
+        continue
+    elif soil_to_fertilizer_started and x[0] != 'fertilizer-to-water':
+        soil_to_fertilizer_map.append(x)
+
+    elif x[0] == 'fertilizer-to-water':
+        soil_to_fertilizer_started = False
+        fertilizer_to_water_started = True
+        continue
+    elif fertilizer_to_water_started and x[0] != 'water-to-light':
+        fertilizer_to_water_map.append(x)
+
+    elif x[0] == 'water-to-light':
+        fertilizer_to_water_started = False
+        water_to_light_started = True
+        continue
+    elif water_to_light_started and x[0] != 'light-to-temperature':
+        water_to_light_map.append(x)
+
+    elif x[0] == 'light-to-temperature':
+        water_to_light_started = False
+        light_to_temperature_started = True
+        continue
+    elif light_to_temperature_started and x[0] != 'temperature-to-humidity':
+        light_to_temperature_map.append(x)
+
+    elif x[0] == 'temperature-to-humidity':
+        light_to_temperature_started = False
+        temperature_to_humidity_started = True
+        continue
+    elif temperature_to_humidity_started and x[0] != 'humidity-to-location':
+        temperature_to_humidity_map.append(x)
+
+    elif x[0] == 'humidity-to-location':
+        temperature_to_humidity_started = False
+        continue
+    else:
+        humidity_to_location_map.append(x)
+
+print(f'''
+Seed list: {seed_list}
+Lists of all mappings:
+- 1. seed-to-soil: {seed_to_soil_map}
+- 2. soil-to-fertilizer: {soil_to_fertilizer_map}
+- 3. fertilizer_to_water: {fertilizer_to_water_map}
+- 4. water-to-light: {water_to_light_map}
+- 5. light-to-temperature: {light_to_temperature_map}
+- 6. temperature-to-humidity: {temperature_to_humidity_map}
+- 7. humidity-to-location: {humidity_to_location_map}
+''')
+        
+
 
